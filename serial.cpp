53d5edd620ced6309ed4f076c621e4745f5dff35
Miguel Martins Duarte <miguel.duarte20@hotmail.com>
2016-03-30 14:58:53 +0100

    Simplified code

--- serial.cpp
+++ serial.cpp
@@ -6,20 +6,11 @@ using namespace std;
 
 #define MAX_CLAUSES (2 << 16) + 1
 #define MAX_VARS 128
+#define MAX_VARS_PER_CLAUSE 21
 
 int N, C, best = 0, nbest = 0;
 bool bestAssignment[MAX_VARS];
 int* clauses[MAX_CLAUSES];
-
-bool comp(int* a, int* b) {
-	// a[0] returns number of clauses in clause
-	return abs(a[a[0]]) < abs(b[b[0]]); 
-}
-
-
-bool intComp(int a, int b) {
-	return abs(a) < abs(b); 
-}
 
 int calcClauses(int vi, bool* vars){
 	int sum = 0;
@@ -37,7 +28,7 @@ int calcClauses(int vi, bool* vars){
 					sum++;
 					break;
 				}
-			} 
+			}
 			
 		}
 	}
@@ -47,29 +38,21 @@ int calcClauses(int vi, bool* vars){
 // unsatisfiable closed clauses
 int calcClosedClauses(int vi, bool* vars){
 	int sum = 0;
-	bool passed = false;
 	for(int i=0; i < C; i++){
-		passed = false;
-		for(int v=1; v <= clauses[i][0]; v++){
+		int v;
+		for(v=1; v <= clauses[i][0]; v++){
 			int c = clauses[i][v];
 			int a = abs(c);
 			if(a >= vi){
-				passed = true;
 				break;
 			}else if(c < 0){
-				if(!vars[a]){					
-					passed = true;
-					break;
-				}				
+				if(!vars[a]) break;
 			}else{
-				if(vars[a]){
-					passed = true;					
-					break;
-				}
-			} 
+				if( vars[a]) break;
+			}
 			
 		}
-		if(!passed) sum++;
+		if(v > clauses[i][0]) sum++;
 	}
 	return sum; 
 }
@@ -103,21 +86,18 @@ void branch(int vi, bool* vars){
 
 int main(){
 	cin >> N >> C;
-	int c;
 	for(int i = 0; i < C; i++){
 		// 1 for size, 1 for last zero
-		clauses[i] = new int[MAX_CLAUSES+2];
+		clauses[i] = new int[MAX_VARS_PER_CLAUSE];
 		for(int v = 1;;v++){
 			cin >> clauses[i][v];
 			if(clauses[i][v] == 0){
 				clauses[i][0]= v - 1;
-				sort(clauses[i]+1, clauses[i]+ v, intComp);
 				break;
 			}
 
 		}
 	}
-	sort(clauses, clauses + C, comp);
 	bool v[MAX_VARS+1];
 	branch(1, v);
 	cout << best << " " << nbest << endl;
