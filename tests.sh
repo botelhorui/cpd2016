2c328c3d30b3a1939e098289a173dc1875432a74
Miguel Martins Duarte <miguel.duarte20@hotmail.com>
2016-04-01 15:39:32 +0100

    Added script with tests

---
+++ tests.sh
@@ -0,0 +1,14 @@
+time ./serial < input/ex3.in
+time OMP_NUM_THREADS=2 ./parallel < input/ex3.in
+time OMP_NUM_THREADS=3 ./parallel < input/ex3.in
+time OMP_NUM_THREADS=4 ./parallel < input/ex3.in
+
+time ./serial < input/ex4.in
+time OMP_NUM_THREADS=2 ./parallel < input/ex4.in
+time OMP_NUM_THREADS=3 ./parallel < input/ex4.in
+time OMP_NUM_THREADS=4 ./parallel < input/ex4.in
+
+time ./serial < input/ex5.in
+time OMP_NUM_THREADS=2 ./parallel < input/ex5.in
+time OMP_NUM_THREADS=3 ./parallel < input/ex5.in
+time OMP_NUM_THREADS=4 ./parallel < input/ex5.in
