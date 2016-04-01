09fdbe02433f77569dfbad3003484b16a02ac6ab
Miguel Martins Duarte <miguel.duarte20@hotmail.com>
2016-04-02 00:25:28 +0100

    Updated tests

--- tests.sh
+++ tests.sh
@@ -2,13 +2,19 @@ time ./serial < input/ex3.in
 time OMP_NUM_THREADS=2 ./parallel < input/ex3.in
 time OMP_NUM_THREADS=3 ./parallel < input/ex3.in
 time OMP_NUM_THREADS=4 ./parallel < input/ex3.in
+time OMP_NUM_THREADS=6 ./parallel < input/ex3.in
+time OMP_NUM_THREADS=8 ./parallel < input/ex3.in
 
 time ./serial < input/ex4.in
 time OMP_NUM_THREADS=2 ./parallel < input/ex4.in
 time OMP_NUM_THREADS=3 ./parallel < input/ex4.in
 time OMP_NUM_THREADS=4 ./parallel < input/ex4.in
+time OMP_NUM_THREADS=6 ./parallel < input/ex4.in
+time OMP_NUM_THREADS=8 ./parallel < input/ex4.in
 
 time ./serial < input/ex5.in
 time OMP_NUM_THREADS=2 ./parallel < input/ex5.in
 time OMP_NUM_THREADS=3 ./parallel < input/ex5.in
 time OMP_NUM_THREADS=4 ./parallel < input/ex5.in
+time OMP_NUM_THREADS=6 ./parallel < input/ex5.in
+time OMP_NUM_THREADS=8 ./parallel < input/ex5.in
