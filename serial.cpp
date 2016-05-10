5901fde95a3b20c4396abd1267ac77f743211d7c
rui <botelhorui@gmail.com>
2016-05-10 19:22:25 +0100

    distribution working

--- serial.cpp
+++ serial.cpp
@@ -9,13 +9,13 @@ using namespace std;
 #define MAX_VARS 128
 #define MAX_VARS_PER_CLAUSE 21
 
-int N, C, best = 0, nbest = 0;
+int NUM_VARS, NUM_CLAUSES, best = 0, nbest = 0;
 bool bestAssignment[MAX_VARS];
 int* clauses[MAX_CLAUSES];
 
 int calcClauses(int vi, bool* vars){
 	int sum = 0;
-	for(int i=0; i < C; i++){
+	for(int i=0; i < NUM_CLAUSES; i++){
 		for(int v=1; v <= clauses[i][0]; v++){
 			int c = clauses[i][v];
 			int a = abs(c);
@@ -39,7 +39,7 @@ int calcClauses(int vi, bool* vars){
 // unsatisfiable closed clauses
 int calcClosedClauses(int vi, bool* vars){
 	int sum = 0;
-	for(int i=0; i < C; i++){
+	for(int i=0; i < NUM_CLAUSES; i++){
 		int v;
 		for(v=1; v <= clauses[i][0]; v++){
 			int c = clauses[i][v];
@@ -62,12 +62,12 @@ int calcClosedClauses(int vi, bool* vars){
 // vars saves variables assignments
 void branch(int vi, bool* vars){
 	
-	if(vi == N+1){
+	if(vi == NUM_VARS+1){
 		int sum = calcClauses(vi, vars);
 		if(sum > best){
 			best = sum;
 			nbest = 1;
-			memcpy(bestAssignment, vars, (N+1) * sizeof(bool));
+			memcpy(bestAssignment, vars, (NUM_VARS+1) * sizeof(bool));
 			/*for(int i=1; i <= N; i++){
 				bestAssignment[i] = vars[i];
 			}*/
@@ -76,7 +76,7 @@ void branch(int vi, bool* vars){
 		}
 		return;
 	}
-	if(C - calcClosedClauses(vi,vars) < best){
+	if(NUM_CLAUSES - calcClosedClauses(vi,vars) < best){
  		return;
 	}
 
@@ -89,8 +89,8 @@ void branch(int vi, bool* vars){
 int main(){
 	int start = omp_get_wtime();
 
-	cin >> N >> C;
-	for(int i = 0; i < C; i++){
+	cin >> NUM_VARS >> NUM_CLAUSES;
+	for(int i = 0; i < NUM_CLAUSES; i++){
 		// 1 for size, 1 for last zero
 		clauses[i] = new int[MAX_VARS_PER_CLAUSE];
 		for(int v = 1;;v++){
@@ -108,9 +108,9 @@ int main(){
 	cout << "Serial time: " << omp_get_wtime() - start << " seconds." << endl;
 
 	cout << best << " " << nbest << endl;
-	for(int i=1; i <= N; i++){
+	for(int i=1; i <= NUM_VARS; i++){
 		cout << (bestAssignment[i] ? i: -i);
-		if(i < N) cout << " ";
+		if(i < NUM_VARS) cout << " ";
 	}
 	cout << endl;
 }
